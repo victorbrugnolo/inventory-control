9a17971f6c3ab299b50703b104e74bdc0ea11eee
victorbrugnolo <brugnolovictor@gmail.com>
2021-04-18 20:40:27 -0300

    handling exceptions on ProductServiceImpl

--- src/main/java/dev/victorbrugnolo/inventorycontrol/api/repositories/ProductRepository.java
+++ src/main/java/dev/victorbrugnolo/inventorycontrol/api/repositories/ProductRepository.java
@@ -1,9 +1,12 @@
 package dev.victorbrugnolo.inventorycontrol.api.repositories;
 
 import dev.victorbrugnolo.inventorycontrol.api.entities.Product;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+  Optional<Product> findByCode(String code);
 
 }
--- src/main/java/dev/victorbrugnolo/inventorycontrol/api/services/impl/ProductServiceImpl.java
+++ src/main/java/dev/victorbrugnolo/inventorycontrol/api/services/impl/ProductServiceImpl.java
@@ -2,9 +2,10 @@ package dev.victorbrugnolo.inventorycontrol.api.services.impl;
 
 import dev.victorbrugnolo.inventorycontrol.api.dtos.ProductDTO;
 import dev.victorbrugnolo.inventorycontrol.api.entities.Product;
+import dev.victorbrugnolo.inventorycontrol.api.exceptions.BadRequestException;
+import dev.victorbrugnolo.inventorycontrol.api.exceptions.NotFoundException;
 import dev.victorbrugnolo.inventorycontrol.api.repositories.ProductRepository;
 import dev.victorbrugnolo.inventorycontrol.api.services.ProductService;
-import java.util.Optional;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,17 +16,25 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
+  private static final String PRODUCT_ALREADY_EXISTS = "Product with code %s already exists";
+  private static final String PRODUCT_NOT_FOUND = "Product not found";
+
   private final ProductRepository productRepository;
 
   @Override
   public Product save(final ProductDTO toSave) {
+    productRepository.findByCode(toSave.getCode())
+        .ifPresent(p -> {
+          throw new BadRequestException(String.format(PRODUCT_ALREADY_EXISTS, toSave.getCode()));
+        });
+
     return productRepository.save(ProductDTO.toEntity(toSave));
   }
 
   @Override
   public Product getById(final String id) {
-    Optional<Product> found = productRepository.findById(UUID.fromString(id));
-    return found.orElse(null);
+    return productRepository.findById(UUID.fromString(id))
+        .orElseThrow(() -> new NotFoundException(PRODUCT_NOT_FOUND));
   }
 
   @Override
@@ -34,15 +43,14 @@ public class ProductServiceImpl implements ProductService {
   }
 
   @Override
-  public Product update(String id, ProductDTO update) {
-    Optional<Product> found = productRepository.findById(UUID.fromString(id));
-    found.get().update(update);
-    return productRepository.save(found.get());
+  public Product update(final String id, final ProductDTO update) {
+    Product found = getById(id);
+    found.update(update);
+    return productRepository.save(found);
   }
 
   @Override
-  public void delete(String id) {
-    Optional<Product> found = productRepository.findById(UUID.fromString(id));
-    productRepository.delete(found.get());
+  public void delete(final String id) {
+    productRepository.delete(getById(id));
   }
 }
